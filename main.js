9b490c25bd1efe0c589a40752c0b5e245403f13a
zahrafayya <zahra.fayyadiyati@gmail.com>
2022-09-14 15:11:13 +0700

    gambar 3 titik

--- main.js
+++ main.js
@@ -5,13 +5,25 @@ function main()
     // gl itu seperti kuas, cat, palet, dan alat-alat lainnya untuk menggambar
     var gl = kanvas.getContext("webgl");
 
+    var vertices = [
+        0.5, 0.5, 
+        0.0, 0.0, 
+        -0.5, 0.5
+    ];
+
+    // pindahin vertices ke GPU dari CPU
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
     // Vertex Shader
     var vertexShaderCode = /* yang akan ditulis di dalam sini adalah source code .glsl */
     `
+    attribute vec2 aPosition;
     void main()
     {
-        float x = 0.0;
-        float y = 0.0;
+        float x = aPosition.x;
+        float y = aPosition.y;
         gl_PointSize = 10.0;
         gl_Position =  vec4(x, y, 0.0, 1.0); 
 
@@ -50,8 +62,15 @@ function main()
     // alat (kuas) dipakai
     gl.useProgram(shaderProgram);
 
+    // Mengajari GPU bagaimana cara mengoleksi nilai 
+    // posisi dari ARRAY_BUFFER untuk setiap vertex
+    // yang sedang diproses
+    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+
     gl.clearColor(1.0, 0.65, 0, 1.0); // values of red, green, blue, alpha
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.POINTS, 0, 1);
+    gl.drawArrays(gl.POINTS, 0, 3);
 }
