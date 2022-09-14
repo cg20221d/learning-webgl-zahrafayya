da76d4e45dfb3f023404fa56ce083ab36133749e
zahrafayya <zahra.fayyadiyati@gmail.com>
2022-09-14 14:51:31 +0700

    vertex & fragment shader

--- main.js
+++ main.js
@@ -6,10 +6,16 @@ function main()
     var gl = kanvas.getContext("webgl");
 
     // Vertex Shader
-    var vertexShaderCode = `
+    var vertexShaderCode = /* yang akan ditulis di dalam sini adalah source code .glsl */
+    `
     void main()
     {
+        float x = 0.0;
+        float y = 0.0;
+        gl_PointSize = 10.0;
+        gl_Position =  vec4(x, y, 0.0, 1.0); 
 
+        // vec4. 4 di sana adalah yang dimaksud di ppt "setiap lambang 2,3,4 di vec yang menggambarkan dimensi"
     }
     `;
     var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
@@ -18,8 +24,13 @@ function main()
 
     // Fragment Shader
     var fragmentShaderCode = `
+    precision mediump float;
     void main()
     {
+        float r = 0.0;
+        float g = 0.0;
+        float b = 1.0;
+        gl_FragColor = vec4(r, g, b, 1.0);
 
     }
     `;
@@ -39,6 +50,8 @@ function main()
     // alat (kuas) dipakai
     gl.useProgram(shaderProgram);
 
-    gl.clearColor(0.5, 0.5, 0.5, 1.0); // values of red, green, blue, alpha
+    gl.clearColor(1.0, 0.65, 0, 1.0); // values of red, green, blue, alpha
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINTS, 0, 1);
 }
